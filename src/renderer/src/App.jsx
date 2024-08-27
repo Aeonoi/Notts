40c2ec83cfe4354e828ae88b6b5e8bc8a626aa1d
Aeonoi <dzhou527@gmail.com>
2024-08-26 22:00:26 -0400

    Starter code

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import './app.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1> Hello World</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/renderer/src/main.jsx
@@ -0,0 +1,11 @@
+import './assets/background.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
