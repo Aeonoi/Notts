d76b6d2ca230143702ca63572c89e0388a2b6a69
Aeonoi <dzhou527@gmail.com>
2025-07-06 11:17:48 -0400

    chore: formatting

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -3,27 +3,26 @@ import { defineConfig, externalizeDepsPlugin } from "electron-vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin()],
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()],
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        "@renderer": resolve("src/renderer/src"),
-      },
-    },
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: "http://localhost:5000",
-
-          changeOrigin: true,
-        },
-      },
-    },
-  },
+	main: {
+		plugins: [externalizeDepsPlugin()],
+	},
+	preload: {
+		plugins: [externalizeDepsPlugin()],
+	},
+	renderer: {
+		resolve: {
+			alias: {
+				"@renderer": resolve("src/renderer/src"),
+			},
+		},
+		plugins: [react()],
+		server: {
+			proxy: {
+				"/api": {
+					target: "http://localhost:5000",
+					changeOrigin: true,
+				},
+			},
+		},
+	},
 });
